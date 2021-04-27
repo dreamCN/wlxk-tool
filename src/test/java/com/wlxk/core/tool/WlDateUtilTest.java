725624f1f343ac1931f5a0e4af15dcb91454d604
sxz <i_sxingzhi@3songshu.com>
2021-04-27 17:02:07 +0800

    调整

---
+++ src/test/java/com/wlxk/core/tool/WlDateUtilTest.java
@@ -0,0 +1,16 @@
+package com.wlxk.core.tool;
+
+import org.junit.Test;
+
+/**
+ * @author sxz
+ * @date 2021/4/27 14:23
+ */
+public class WlDateUtilTest {
+
+    @Test
+    public void test() {
+        System.out.println(WlDateUtil.getCurrentDayBegin());
+    }
+
+}
